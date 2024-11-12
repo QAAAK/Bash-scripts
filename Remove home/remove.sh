75507a7ec99460fe3b7873d0758d361456ef9222
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-11-12 12:55:36 +0300

    Create remove.sh

---
+++ Remove home/remove.sh
@@ -0,0 +1,8 @@
+!#bin/bash
+
+sudo kill -9 $(lsof /home | awk '{print $2}')
+
+
+sudo find /home/* -maxdepth 1 -type l | sudo xargs rm
+sudo umount /home && sudo rm -rf /home
+sudo ln -s /data/home /home
