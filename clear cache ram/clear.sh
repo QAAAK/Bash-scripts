c347f8cb2a70190f18702bb70cb30e2277ab27e7
Dmitry Santalov <dmitsantalov@yandex.ru>
2025-01-10 09:52:07 +0300

    Create clear.sh

---
+++ clear cache ram/clear.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Скрипт проверяет объём кэша в оперативной памяти и при превышении в 1Gb предлагает запустить очистку
+
+cache=$(free -m | grep Mem | awk '{print $6}')
+
+echo "Cache $cache MB"
+
+if [ $cache -ge 1000 ]
+then
+  echo "cache > 1 GB"
+  echo ""
+  read -p "Ready? Y/n: " ansver
+    case $ansver in
+    [y,Y])
+      echo "please password"
+      sudo echo "it was occupied by memory"
+      free -m
+      sync
+      echo 1 | sudo tee -a /proc/sys/vm/drop_caches
+      sync
+      echo 2 | sudo tee -a /proc/sys/vm/drop_caches
+      sync
+      echo 3 | sudo tee -a /proc/sys/vm/drop_caches
+      free -m;;
+    [n,N])
+      echo "Exit";;
+    esac
+else
+  echo "enough free memory"
+fi
